f1d73cc3f6556c61eb59ee65b198654e2346f2bd
Sung J Byun <sungjbyun@D-10-19-176-4.dhcp4.washington.edu>
2018-05-03 17:42:46 -0700

    working on dialog box

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { AppBar, Toolbar, Typography } from 'material-ui';
 import CreateDialog from '../excercises/Dialog/Create';
 
-export default props => (
+export default ({ muscles, onExcerciseCreate }) => (
 
   <AppBar position="static">
     <Toolbar>
       <Typography variant="headline" color="inherit" style={{ flex: 1 }}>
         Excercises
       </Typography>
-      <CreateDialog />
+      <CreateDialog
+        muscles={muscles}
+        onCreate={onExcerciseCreate}
+      />
     </Toolbar>
   </AppBar>
 
--- src/components/excercises/Dialog/Create.js
+++ src/components/excercises/Dialog/Create.js
@@ -1,5 +1,5 @@
 import React, { Fragment, Component } from 'react';
-import { Button, Dialog } from 'material-ui';
+import { Button, Dialog, TextField, Select } from 'material-ui';
 // import TextField from 'material-ui/TextField';
 import {
   DialogActions,
@@ -8,11 +8,25 @@ import {
   DialogTitle,
 } from 'material-ui/Dialog';
 import { Add } from 'material-ui-icons'; // need to import separately // docs not working, do this way!
-// import Icon from 'material-ui/Icon';
+import { InputLabel } from 'material-ui/Input';
+import { MenuItem } from 'material-ui/Menu';
+import { FormControl } from 'material-ui/Form';
+import { withStyles } from 'material-ui/styles';
 
-export default class extends Component {
+const styles = theme => ({
+  FormControl: {
+    width: 500,
+  },
+});
+
+export default withStyles(styles)(class extends Component {
   state = {
     open: false,
+    excercise: {
+      title: '',
+      description: '',
+      muscles: '',
+    }
   }
 
   handleToggle = () => {
@@ -21,8 +35,26 @@ export default class extends Component {
      })
   }
 
+  handleChange = name => ({target: {value}}) => {
+    this.setState({
+      excercise: {
+        ...this.state.excercise,
+        [name]: value,
+      }
+    });
+  };
+
+  handleSubmit = () => {
+    // TODO: validation
+    const { excercise } = this.state
+
+    this.props.onCreate(excercise)
+    })
+  }
+
   render() {
-    const {open} = this.state
+    const {open, excercise: {title, description, muscles}} = this.state,
+          {classes, muscles: categories} = this.props
     return (
       <Fragment>
         <Button variant="fab" onClick={this.handleToggle} mini>
@@ -44,11 +76,47 @@ export default class extends Component {
           <DialogContent>
             <DialogContentText>
            Please fill out the form below.
+           <form>
+           <TextField
+            label="Title"
+            value={title}
+            onChange={this.handleChange('title')}
+            margin="normal"
+            className={classes.FormControl}
+          />
+          <br />
+          <FormControl>
+          <InputLabel htmlFor="muscles">Muscles</InputLabel>
+          <Select
+            value={muscles}
+            onChange={this.handleChange('muscles')}
+            className={classes.FormControl}
+          >
+            {categories.map(category => {
+            <MenuItem value={category}>{category}</MenuItem>
+            })}
+          </Select>
+          <br />
+        </FormControl>
+          <TextField
+            multiline
+            rows='4'
+            label="Description"
+            value={description}
+            onChange={this.handleChange('description')}
+            margin="normal"
+            className={classes.FormControl}
+          />
+          <br />
+           </form>
             </DialogContentText>
 
           </DialogContent>
           <DialogActions>
-            <Button color="primary" variant='raised'>
+            <Button
+              color="primary"
+              variant='raised'
+              onClick={this.handleSubmit}>
                 Create
             </Button>
           </DialogActions>
@@ -57,4 +125,4 @@ export default class extends Component {
     );
   }
 }
-
+)
